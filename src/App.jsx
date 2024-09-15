1084137fada7f3b2b614229d13dee1c330bc49d2
Wanderley01 <edioneto00@gmail.com>
2024-09-15 13:44:20 -0300

    Segmentação da tela em div's

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
+import './styles/Main.css';
+import './App.css';
 
-import './App.css'
 
 function App() {
 
   return (
     <>
-      
+    <div className='container'>
+      <div className='containercard'>
+        <h1>PT1</h1>
+        <h1>PT1</h1>
+        <h1>PT1</h1>
+        </div>
+
+        <div className='complaint'>
+        <h1>PT2</h1>
+        <h1>PT2</h1>
+        <h1>PT2</h1>
+        </div>
+    </div>
     </>
   )
 }
